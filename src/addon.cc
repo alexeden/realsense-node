0060b136ade0223fd2b67f6fae499979342ede2c
Alex Eden <alexandereden91@gmail.com>
2019-08-01 21:13:00 -0500

    Include the lib files in addon

--- src/addon.cc
+++ src/addon.cc
@@ -1,4 +1,9 @@
-#	include <napi.h>
+#include <librealsense2/h/rs_internal.h>
+#include <librealsense2/h/rs_pipeline.h>
+#include <librealsense2/hpp/rs_types.hpp>
+#include <librealsense2/rs.h>
+
+#include <napi.h>
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
 	return exports;
