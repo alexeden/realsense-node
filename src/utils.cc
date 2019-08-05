5ff9c25d5b0ad05e2e5cff974d84c0e60318dee0
Alex Eden <alexandereden91@gmail.com>
2019-08-05 00:30:22 -0500

    Move CallNativeMethod to a util file

---
+++ src/utils.cc
@@ -0,0 +1,25 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <librealsense2/hpp/rs_types.hpp>
+
+template<typename R, typename F, typename... arguments>
+R GetNativeResult(F func, rs2_error** error, arguments... params) {
+	// reset the error pointer for each call.
+	*error = nullptr;
+	ErrorUtil::ResetError();
+	R val = func(params...);
+	ErrorUtil::AnalyzeError(*error);
+	return val;
+}
+
+template<typename F, typename... arguments>
+void CallNativeFunc(F func, rs2_error** error, arguments... params) {
+	// reset the error pointer for each call.
+	*error = nullptr;
+	ErrorUtil::ResetError();
+	func(params...);
+	ErrorUtil::AnalyzeError(*error);
+}
+
+#endif
