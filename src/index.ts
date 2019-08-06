b2893fbbd7fa631d9b0bad35b7abf40db5989e49
Alex Eden <alexandereden91@gmail.com>
2019-08-05 22:18:11 -0500

    Register an error callback

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import { RealSenseAddon } from './types';
 
 const addon: RealSenseAddon = require('bindings')('realsense_node');
 
+addon.registerErrorCallback(
+  {
+    callback(error: Error) {
+      console.error(`Native module error!`, error);
+    },
+  },
+  'callback'
+);
+
 export {
   addon,
 };
