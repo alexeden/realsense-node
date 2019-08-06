f77294b2ab8bec526c9e219ad5d053b113c7538f
Alex Eden <alexandereden91@gmail.com>
2019-08-06 02:02:56 -0500

    Define Options and RangeOptions

--- src/options.cc
+++ src/options.cc
@@ -1,6 +1,25 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
+#include <napi.h>
+#include <librealsense2/hpp/rs_types.hpp>
+#include "utils.cc"
+#include "dict_base.cc"
+
+using namespace Napi;
+
+class RSOptionRange : public DictBase {
+ public:
+  RSOptionRange(Env env, float min, float max, float step, float def) : DictBase(env) {
+    SetMemberT("minValue", min);
+    SetMemberT("maxValue", max);
+    SetMemberT("step", step);
+    SetMemberT("defaultValue", def);
+  }
+};
+
+
+
 class Options {
   public:
 	Options()
@@ -13,51 +32,53 @@ class Options {
 
 	virtual rs2_options* GetOptionsPointer() = 0;
 
-	void SupportsOptionInternal(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-		int32_t option = info[0]->IntegerValue();
+	Napi::Value SupportsOptionInternal(const CallbackInfo& info) {
+		int32_t option = info[0].ToNumber().Int32Value();
 		auto on		   = GetNativeResult<
 			 int>(rs2_supports_option, &error_, GetOptionsPointer(), static_cast<rs2_option>(option), &error_);
-		info.GetReturnValue().Set(Nan::New(on ? true : false));
-		return;
+
+		return Boolean::New(info.Env(), on ? true : false);
 	}
 
-	void GetOptionInternal(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-		int32_t option = info[0]->IntegerValue();
+	Napi::Value GetOptionInternal(const CallbackInfo& info) {
+		int32_t option = info[0].ToNumber().Int32Value();
 		auto value	 = GetNativeResult<
 		  float>(rs2_get_option, &error_, GetOptionsPointer(), static_cast<rs2_option>(option), &error_);
-		info.GetReturnValue().Set(Nan::New(value));
+
+		return Number::New(info.Env(), value);
 	}
 
-	void GetOptionDescriptionInternal(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-		int32_t option = info[0]->IntegerValue();
+	Napi::Value GetOptionDescriptionInternal(const CallbackInfo& info) {
+		int32_t option = info[0].ToNumber().Int32Value();
 		auto desc	  = GetNativeResult<
 		   const char*>(rs2_get_option_description, &error_, GetOptionsPointer(), static_cast<rs2_option>(option), &error_);
+
 		if (desc)
-			info.GetReturnValue().Set(Nan::New(desc).ToLocalChecked());
+			return String::New(info.Env(), desc);
 		else
-			info.GetReturnValue().Set(Nan::Undefined());
+			return info.Env().Undefined();
 	}
 
-	void GetOptionValueDescriptionInternal(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-		int32_t option = info[0]->IntegerValue();
-		auto val	   = info[1]->NumberValue();
+	Napi::Value GetOptionValueDescriptionInternal(const CallbackInfo& info) {
+		int32_t option = info[0].ToNumber().Int32Value();
+		auto val	   = info[1].ToNumber().Int32Value();
 		auto desc	  = GetNativeResult<
 		   const char*>(rs2_get_option_value_description, &error_, GetOptionsPointer(), static_cast<rs2_option>(option), val, &error_);
 		if (desc)
-			info.GetReturnValue().Set(Nan::New(desc).ToLocalChecked());
+			return String::New(info.Env(), desc);
 		else
-			info.GetReturnValue().Set(Nan::Undefined());
+			return info.Env().Undefined();
 	}
 
-	void SetOptionInternal(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-		int32_t option = info[0]->IntegerValue();
-		auto val	   = info[1]->NumberValue();
+	Napi::Value SetOptionInternal(const CallbackInfo& info) {
+		int32_t option = info[0].ToNumber().Int32Value();
+		auto val	   = info[1].ToNumber().Int32Value();
 		CallNativeFunc(rs2_set_option, &error_, GetOptionsPointer(), static_cast<rs2_option>(option), val, &error_);
-		info.GetReturnValue().Set(Nan::Undefined());
+		return info.Env().Undefined();
 	}
 
-	void GetOptionRangeInternal(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-		int32_t option = info[0]->IntegerValue();
+	Napi::Value GetOptionRangeInternal(const CallbackInfo& info) {
+		int32_t option = info[0].ToNumber().Int32Value();
 		float min	  = 0;
 		float max	  = 0;
 		float step	 = 0;
@@ -72,14 +93,16 @@ class Options {
 		  &step,
 		  &def,
 		  &error_);
-		info.GetReturnValue().Set(RSOptionRange(min, max, step, def).GetObject());
+
+		return RSOptionRange(info.Env(), min, max, step, def).GetObject();
 	}
 
-	void IsOptionReadonlyInternal(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-		int32_t option = info[0]->IntegerValue();
+	Napi::Value IsOptionReadonlyInternal(const CallbackInfo& info) {
+		int32_t option = info[0].ToNumber().Int32Value();
 		auto val	   = GetNativeResult<
 			int>(rs2_is_option_read_only, &error_, GetOptionsPointer(), static_cast<rs2_option>(option), &error_);
-		info.GetReturnValue().Set(Nan::New((val) ? true : false));
+
+		return Boolean::New(info.Env(), val ? true : false);
 	}
 
   private:
