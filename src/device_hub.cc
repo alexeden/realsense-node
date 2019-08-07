879682ae1795225062d6e94fbaf3eb59f522ff17
Alex Eden <alexandereden91@gmail.com>
2019-08-06 22:01:01 -0500

    Convert most of RSDeviceHub

--- src/device_hub.cc
+++ src/device_hub.cc
@@ -1,103 +1,102 @@
 #ifndef DEVICE_HUB_H
 #define DEVICE_HUB_H
 
+#include "device.cc"
+#include <librealsense2/hpp/rs_types.hpp>
 #include <napi.h>
 
-class RSDeviceHub : public Nan::ObjectWrap {
- public:
-  static void Init(v8::Local<v8::Object> exports) {
-    v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
-    tpl->SetClassName(Nan::New("RSDeviceHub").ToLocalChecked());
-    tpl->InstanceTemplate()->SetInternalFieldCount(1);
+using namespace Napi;
 
-    Nan::SetPrototypeMethod(tpl, "waitForDevice", WaitForDevice);
-    Nan::SetPrototypeMethod(tpl, "isConnected", IsConnected);
-    Nan::SetPrototypeMethod(tpl, "destroy", Destroy);
-    constructor_.Reset(tpl->GetFunction());
-    exports->Set(Nan::New("RSDeviceHub").ToLocalChecked(),
-      tpl->GetFunction());
-  }
+class RSDeviceHub : public ObjectWrap<RSDeviceHub> {
+  public:
+	static Object Init(Napi::Env env, Object exports) {
+		Napi::Function func = DefineClass(
+		  env,
+		  "RSDeviceHub",
+		  {
+			InstanceMethod("waitForDevice", &RSDeviceHub::WaitForDevice),
+			InstanceMethod("isConnected", &RSDeviceHub::IsConnected),
+			InstanceMethod("destroy", &RSDeviceHub::Destroy),
+		  });
 
-  static v8::Local<v8::Object> NewInstance() {
-    Nan::EscapableHandleScope scope;
+		constructor = Napi::Persistent(func);
+		constructor.SuppressDestruct();
+		exports.Set("RSDeviceHub", func);
 
-    v8::Local<v8::Function> cons = Nan::New<v8::Function>(constructor_);
-    v8::Local<v8::Context> context =
-        v8::Isolate::GetCurrent()->GetCurrentContext();
+		return exports;
+	}
 
-    v8::Local<v8::Object> instance =
-        cons->NewInstance(context, 0, nullptr).ToLocalChecked();
+	static Object NewInstance(Napi::Env env) {
+		EscapableHandleScope scope(env);
+		Object instance = constructor.New({});
 
-    return scope.Escape(instance);
-  }
+		return scope.Escape(napi_value(instance)).ToObject();
+	}
 
- private:
-  RSDeviceHub() : hub_(nullptr), ctx_(nullptr), error_(nullptr) {}
+	RSDeviceHub(const CallbackInfo& info)
+	  : ObjectWrap<RSDeviceHub>(info)
+	  , hub_(nullptr)
+	  , ctx_(nullptr)
+	  , error_(nullptr) {
+	}
 
-  ~RSDeviceHub() {
-    DestroyMe();
-  }
+	~RSDeviceHub() {
+		DestroyMe();
+	}
 
-  void DestroyMe() {
-    if (error_) rs2_free_error(error_);
-    error_ = nullptr;
+  private:
+	void DestroyMe() {
+		if (error_) rs2_free_error(error_);
+		error_ = nullptr;
 
-    if (hub_) rs2_delete_device_hub(hub_);
-    hub_ = nullptr;
-    ctx_ = nullptr;
-  }
+		if (hub_) rs2_delete_device_hub(hub_);
+		hub_ = nullptr;
+		ctx_ = nullptr;
+	}
 
-  static NAN_METHOD(Destroy) {
-    auto me = Nan::ObjectWrap::Unwrap<RSDeviceHub>(info.Holder());
-    if (me) me->DestroyMe();
-    info.GetReturnValue().Set(Nan::Undefined());
-  }
+	Napi::Value Destroy(const CallbackInfo& info) {
+		this->DestroyMe();
+		return info.Env().Undefined();
+	}
 
-  static void New(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-    if (info.IsConstructCall()) {
-      RSDeviceHub* obj = new RSDeviceHub();
-      RSContext* ctx = Nan::ObjectWrap::Unwrap<RSContext>(info[0]->ToObject());
-      obj->ctx_ = ctx->ctx_;
-      obj->hub_ = GetNativeResult<rs2_device_hub*>(rs2_create_device_hub,
-          &obj->error_, obj->ctx_, &obj->error_);
-      obj->Wrap(info.This());
-      info.GetReturnValue().Set(info.This());
-    }
-  }
+	static void New(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+		if (info.IsConstructCall()) {
+			RSDeviceHub* obj = new RSDeviceHub();
+			RSContext* ctx   = Nan::ObjectWrap::Unwrap<RSContext>(info[0]->ToObject());
+			obj->ctx_		 = ctx->ctx_;
+			obj->hub_ = GetNativeResult<rs2_device_hub*>(rs2_create_device_hub, &obj->error_, obj->ctx_, &obj->error_);
+			obj->Wrap(info.This());
+			info.GetReturnValue().Set(info.This());
+		}
+	}
 
-  static NAN_METHOD(WaitForDevice) {
-    info.GetReturnValue().Set(Nan::Undefined());
-    auto me = Nan::ObjectWrap::Unwrap<RSDeviceHub>(info.Holder());
-    if (!me) return;
+	Napi::Value WaitForDevice(const CallbackInfo& info) {
+		auto dev
+		  = GetNativeResult<rs2_device*>(rs2_device_hub_wait_for_device, &this->error_, this->hub_, &this->error_);
+		if (!dev) return info.Env().Undefined();
 
-    auto dev = GetNativeResult<rs2_device*>(rs2_device_hub_wait_for_device,
-        &me->error_, me->hub_, &me->error_);
-    if (!dev) return;
+		return RSDevice::NewInstance(info.Env(), dev);
+	}
 
-    info.GetReturnValue().Set(RSDevice::NewInstance(dev));
-  }
+	Napi::Value IsConnected(const CallbackInfo& info) {
+		auto dev = ObjectWrap<RSDevice>::Unwrap(info[0].ToObject());
+		if (!dev) return;
 
-  static NAN_METHOD(IsConnected) {
-    info.GetReturnValue().Set(Nan::Undefined());
-    auto me = Nan::ObjectWrap::Unwrap<RSDeviceHub>(info.Holder());
-    auto dev = Nan::ObjectWrap::Unwrap<RSDevice>(info[0]->ToObject());
-    if (!me || !dev) return;
+		auto res = GetNativeResult<
+		  int>(rs2_device_hub_is_device_connected, &this->error_, this->hub_, dev->dev_, &this->error_);
+		if (this->error_) return;
 
-    auto res = GetNativeResult<int>(rs2_device_hub_is_device_connected,
-       &me->error_, me->hub_, dev->dev_, &me->error_);
-    if (me->error_) return;
+		return Boolean::New(res ? true : false);
+	}
 
-    info.GetReturnValue().Set(Nan::New(res ? true : false));
-  }
+  private:
+	static FunctionReference constructor;
 
- private:
-  static Nan::Persistent<v8::Function> constructor_;
-
-  rs2_device_hub* hub_;
-  rs2_context* ctx_;
-  rs2_error* error_;
+	rs2_device_hub* hub_;
+	rs2_context* ctx_;
+	rs2_error* error_;
 };
 
-Nan::Persistent<v8::Function> RSDeviceHub::constructor_;
+Napi::FunctionReference RSDeviceHub::constructor;
 
 #endif
