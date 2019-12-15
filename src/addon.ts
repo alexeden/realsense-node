3e059259bb1b8c89979754cbbf8b0fca1709d321
Alex Eden <alexandereden91@gmail.com>
2019-12-15 17:18:04 -0600

    Rename the wrapped RS constructors functions

--- src/addon.ts
+++ src/addon.ts
@@ -25,91 +25,91 @@ export const cleanup = () => {
   addon.cleanup();
 };
 
-export const RSAlign = () => {
+export const createAlign = () => {
   const rsAlign = new addon.RSAlign();
   destroyables.push(rsAlign);
 
   return rsAlign;
 };
 
-export const RSColorizer = () => {
+export const createColorizer = () => {
   const rsColorizer = new addon.RSColorizer();
   destroyables.push(rsColorizer);
 
   return rsColorizer;
 };
 
-export const RSConfig = () => {
+export const createConfig = () => {
   const rsConfig = new addon.RSConfig();
   destroyables.push(rsConfig);
 
   return rsConfig;
 };
 
-export const RSContext = () => {
+export const createContext = () => {
   const rsContext = new addon.RSContext();
   destroyables.push(rsContext);
 
   return rsContext;
 };
 
-export const RSDevice = () => {
+export const createDevice = () => {
   const rsDevice = new addon.RSDevice();
   destroyables.push(rsDevice);
 
   return rsDevice;
 };
 
-export const RSDeviceList = () => {
+export const createDeviceList = () => {
   const rsDeviceList = new addon.RSDeviceList();
   destroyables.push(rsDeviceList);
 
   return rsDeviceList;
 };
 
-export const RSFrame = () => {
+export const createFrame = () => {
   const rsFrame = new addon.RSFrame();
   destroyables.push(rsFrame);
 
   return rsFrame;
 };
 
-export const RSFrameSet = () => {
+export const createFrameSet = () => {
   const rsFrameSet = new addon.RSFrameSet();
   destroyables.push(rsFrameSet);
 
   return rsFrameSet;
 };
 
-export const RSPipeline = () => {
+export const createPipeline = () => {
   const rsPipeline = new addon.RSPipeline();
   destroyables.push(rsPipeline);
 
   return rsPipeline;
 };
 
-export const RSPipelineProfile = () => {
+export const createPipelineProfile = () => {
   const rsPipelineProfile = new addon.RSPipelineProfile();
   destroyables.push(rsPipelineProfile);
 
   return rsPipelineProfile;
 };
 
-export const RSSensor = () => {
+export const createSensor = () => {
   const rsSensor = new addon.RSSensor();
   destroyables.push(rsSensor);
 
   return rsSensor;
 };
 
-export const RSStreamProfile = () => {
+export const createStreamProfile = () => {
   const rsStreamProfile = new addon.RSStreamProfile();
   destroyables.push(rsStreamProfile);
 
   return rsStreamProfile;
 };
 
-export const RSSyncer = () => {
+export const createSyncer = () => {
   const rsSyncer = new addon.RSSyncer();
   destroyables.push(rsSyncer);
 
