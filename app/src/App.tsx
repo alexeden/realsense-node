d00600dce6b0ef4fbb8e8ff07cf986deea5052b1
Alex Eden <alexandereden91@gmail.com>
2019-12-20 22:43:09 -0600

    Hello React

---
+++ app/src/App.tsx
@@ -0,0 +1,7 @@
+// tslint:disable-next-line: no-import-side-effect
+import './index.css';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>⚛ electron</h1>, document.getElementById('root'));
